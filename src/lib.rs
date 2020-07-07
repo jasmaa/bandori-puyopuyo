a8e22d34852db8dbf7660cede2891528254271cb
jasmaa <jasonmaa3955@gmail.com>
2020-07-06 22:46:02 -0400

    Add text renderer, fix buggy floodfill

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use std::fmt;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -84,29 +85,35 @@ impl Engine {
             None => 0,
         }
     }
-    fn count_blob_aux(&self, data: &mut Vec<Option<Affiliation>>, row: u32, col: u32, curr: Affiliation) -> u32 {
+    fn count_blob_aux(
+        &self,
+        data: &mut Vec<Option<Affiliation>>,
+        row: u32,
+        col: u32,
+        curr: Affiliation,
+    ) -> u32 {
         let mut count = 0;
         let curr_idx = self.get_index(row, col);
-        match self.affiliation_data[curr_idx] {
+        match data[curr_idx] {
             Some(v) => {
                 if curr == v {
-                    data[curr_idx] = Option::None;
-                }
+                    data[curr_idx] = None;
+                    count += 1;
 
-                // Floodfill
-                if row as i32 - 1 >= 0 {
-                    count += self.count_blob_aux(data, row - 1, col, v);
+                    // Floodfill
+                    if row as i32 - 1 >= 0 {
+                        count += self.count_blob_aux(data, row - 1, col, v);
+                    }
+                    if row + 1 < self.height {
+                        count += self.count_blob_aux(data, row + 1, col, v);
+                    }
+                    if col as i32 - 1 >= 0 {
+                        count += self.count_blob_aux(data, row, col - 1, v);
+                    }
+                    if col + 1 < self.width {
+                        count += self.count_blob_aux(data, row, col + 1, v);
+                    }
                 }
-                if row + 1 <= self.height {
-                    count += self.count_blob_aux(data, row + 1, col, v);
-                }
-                if col as i32 - 1 >= 0 {
-                    count += self.count_blob_aux(data, row, col - 1, v);
-                }
-                if col + 1 <= self.width {
-                    count += self.count_blob_aux(data, row, col + 1, v);
-                }
-
                 count
             }
             None => 0,
@@ -126,23 +133,23 @@ impl Engine {
         match self.affiliation_data[curr_idx] {
             Some(v) => {
                 if curr == v {
-                    self.affiliation_data[curr_idx] = Option::None;
-                    self.sprite_data[curr_idx] = Option::None;
-                    self.direction_data[curr_idx] = Option::None;
-                }
+                    self.affiliation_data[curr_idx] = None;
+                    self.sprite_data[curr_idx] = None;
+                    self.direction_data[curr_idx] = None;
 
-                // Floodfill
-                if row as i32 - 1 >= 0 {
-                    self.clear_blob_aux(row - 1, col, v);
-                }
-                if row + 1 <= self.height {
-                    self.clear_blob_aux(row + 1, col, v);
-                }
-                if col as i32 - 1 >= 0 {
-                    self.clear_blob_aux(row, col - 1, v);
-                }
-                if col + 1 <= self.width {
-                    self.clear_blob_aux(row, col + 1, v);
+                    // Floodfill
+                    if row as i32 - 1 >= 0 {
+                        self.clear_blob_aux(row - 1, col, v);
+                    }
+                    if row + 1 < self.height {
+                        self.clear_blob_aux(row + 1, col, v);
+                    }
+                    if col as i32 - 1 >= 0 {
+                        self.clear_blob_aux(row, col - 1, v);
+                    }
+                    if col + 1 < self.width {
+                        self.clear_blob_aux(row, col + 1, v);
+                    }
                 }
             }
             None => (),
@@ -156,9 +163,17 @@ impl Engine {
         Engine {
             width: width,
             height: height,
-            sprite_data: (0..width * height).map(|_| Option::None).collect(),
-            affiliation_data: (0..width * height).map(|_| Option::None).collect(),
-            direction_data: (0..width * height).map(|_| Option::None).collect(),
+            sprite_data: (0..width * height).map(|_| None).collect(),
+            affiliation_data: (0..width * height)
+                .map(|i| {
+                    if i >= 20 && i < 70 {
+                        Some(Affiliation::Popipa)
+                    } else {
+                        None
+                    }
+                })
+                .collect(),
+            direction_data: (0..width * height).map(|_| None).collect(),
         }
     }
 
@@ -170,5 +185,26 @@ impl Engine {
         self.height
     }
 
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+
     pub fn tick(&mut self) {}
+}
+
+impl fmt::Display for Engine {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.affiliation_data.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = match cell {
+                    Some(v) => format!("{}", v as i32),
+                    None => format!("-"),
+                };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+
+        Ok(())
+    }
 }
--- web/index.js
+++ web/index.js
@@ -1,6 +1,7 @@
 import { Engine } from 'bandori-puyopuyo';
 import { memory } from 'bandori-puyopuyo/bandori_puyopuyo_bg';
 
-const engine = Engine.new(80, 90);
+const engine = Engine.new(10, 20);
 
 console.log(engine.get_width());
+console.log(engine.render());
