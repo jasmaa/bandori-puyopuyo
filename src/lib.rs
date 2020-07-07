c1cda6d8e0cfdc1b01a5eb777fe14db772f0d5a3
jasmaa <jasonmaa3955@gmail.com>
2020-07-06 21:57:46 -0400

    Remove board, add clear and count blob

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-mod board;
 mod utils;
-use board::Board;
 
 use wasm_bindgen::prelude::*;
 
@@ -66,14 +64,89 @@ pub enum Sprite {
 pub struct Engine {
     width: u32,
     height: u32,
-    sprite_board: Board<Sprite>,
-    affiliation_board: Board<Affiliation>,
-    direction_board: Board<Direction>,
+    sprite_data: Vec<Option<Sprite>>,
+    affiliation_data: Vec<Option<Affiliation>>,
+    direction_data: Vec<Option<Direction>>,
 }
 
 impl Engine {
-    pub fn clear() {
+    // Calculates index from grid row and column
+    pub fn get_index(&self, row: u32, col: u32) -> usize {
+        (self.width * row + col) as usize
+    }
 
+    // Counts blob with floodfill
+    pub fn count_blob(&self, row: u32, col: u32) -> u32 {
+        let mut affiliation_data_clone = self.affiliation_data.clone();
+        let curr_idx = self.get_index(row, col);
+        match self.affiliation_data[curr_idx] {
+            Some(v) => self.count_blob_aux(&mut affiliation_data_clone, row, col, v),
+            None => 0,
+        }
+    }
+    fn count_blob_aux(&self, data: &mut Vec<Option<Affiliation>>, row: u32, col: u32, curr: Affiliation) -> u32 {
+        let mut count = 0;
+        let curr_idx = self.get_index(row, col);
+        match self.affiliation_data[curr_idx] {
+            Some(v) => {
+                if curr == v {
+                    data[curr_idx] = Option::None;
+                }
+
+                // Floodfill
+                if row as i32 - 1 >= 0 {
+                    count += self.count_blob_aux(data, row - 1, col, v);
+                }
+                if row + 1 <= self.height {
+                    count += self.count_blob_aux(data, row + 1, col, v);
+                }
+                if col as i32 - 1 >= 0 {
+                    count += self.count_blob_aux(data, row, col - 1, v);
+                }
+                if col + 1 <= self.width {
+                    count += self.count_blob_aux(data, row, col + 1, v);
+                }
+
+                count
+            }
+            None => 0,
+        }
+    }
+
+    // Clears blob with floodfill
+    pub fn clear_blob(&mut self, row: u32, col: u32) {
+        let curr_idx = self.get_index(row, col);
+        match self.affiliation_data[curr_idx] {
+            Some(v) => self.clear_blob_aux(row, col, v),
+            None => (),
+        }
+    }
+    fn clear_blob_aux(&mut self, row: u32, col: u32, curr: Affiliation) {
+        let curr_idx = self.get_index(row, col);
+        match self.affiliation_data[curr_idx] {
+            Some(v) => {
+                if curr == v {
+                    self.affiliation_data[curr_idx] = Option::None;
+                    self.sprite_data[curr_idx] = Option::None;
+                    self.direction_data[curr_idx] = Option::None;
+                }
+
+                // Floodfill
+                if row as i32 - 1 >= 0 {
+                    self.clear_blob_aux(row - 1, col, v);
+                }
+                if row + 1 <= self.height {
+                    self.clear_blob_aux(row + 1, col, v);
+                }
+                if col as i32 - 1 >= 0 {
+                    self.clear_blob_aux(row, col - 1, v);
+                }
+                if col + 1 <= self.width {
+                    self.clear_blob_aux(row, col + 1, v);
+                }
+            }
+            None => (),
+        }
     }
 }
 
@@ -83,9 +156,9 @@ impl Engine {
         Engine {
             width: width,
             height: height,
-            sprite_board: Board::new(width, height),
-            affiliation_board: Board::new(width, height),
-            direction_board: Board::new(width, height),
+            sprite_data: (0..width * height).map(|_| Option::None).collect(),
+            affiliation_data: (0..width * height).map(|_| Option::None).collect(),
+            direction_data: (0..width * height).map(|_| Option::None).collect(),
         }
     }
 
@@ -97,8 +170,5 @@ impl Engine {
         self.height
     }
 
-    pub fn tick(&mut self) {
-
-        self.affiliation_board.floodfill(0, 0, Affiliation::Popipa);
-    }
+    pub fn tick(&mut self) {}
 }
